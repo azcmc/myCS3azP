f134cf31c003d4970ffde9a1342f764768fae590
azcmc <dr.akter.cmc@gmail.com>
2024-11-08 21:07:16 +0600

    Add files via upload

---
+++ Animenosub/src/main/kotlin/com/Animenosub/AnimenosubProvider.kt
@@ -0,0 +1,16 @@
+package com.Animenosub
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class AnimenosubProvider: Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Animenosub())
+        registerExtractorAPI(Vtbe())
+        registerExtractorAPI(waaw())
+        registerExtractorAPI(wishfast())
+        registerExtractorAPI(FileMoonSx())
+    }
+}
