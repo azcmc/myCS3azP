a0a942c992e3e4ddca611308594219111ca48bfa
azcmc <dr.akter.cmc@gmail.com>
2024-11-08 21:09:22 +0600

    Add files via upload

---
+++ Desicinemas/src/main/kotlin/com/Desicinemas/DesicinemasPlugin.kt
@@ -0,0 +1,20 @@
+package com.Desicinemas
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class DesicinemasPlugin: Plugin() {
+    override fun load(context: Context) {
+        // All providers should be added in this manner. Please don't edit the providers list directly.
+        val provider = DesicinemasProvider()
+        registerMainAPI(provider)
+        registerMainAPI(BollyzoneProvider())
+        registerExtractorAPI(Tvlogy((provider.name)))
+        registerExtractorAPI(Tellygossips((provider.name)))
+        registerExtractorAPI(Tvlogyflow((provider.name)))
+    }
+}
+
+
